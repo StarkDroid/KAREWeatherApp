8836913e5c6f8ba05e4d6c298954768dbc6312db
Trishiraj <gamerprince.exp@gmail.com>
2024-01-14 04:30:30 +0530

    Add preview for WeatherCard

--- app/src/main/java/com/kare/weatherapp/ui/component/WeatherCard.kt
+++ app/src/main/java/com/kare/weatherapp/ui/component/WeatherCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kare.weatherapp.R
 import com.kare.weatherapp.model.WeatherDetails
@@ -42,4 +43,14 @@ fun WeatherCard(weatherDetails: WeatherDetails?) {
             )
         }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun WeatherCardPreview() {
+    val dummyWeatherDetail = WeatherDetails(
+        name = "Chennai",
+        main = null
+    )
+    WeatherCard(weatherDetails = dummyWeatherDetail)
 }
